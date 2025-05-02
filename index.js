254aa53b972a75d1f9fdf859429ea93222d289d9
Augment Chinedu <augmentchinedu@gmail.com>
2025-05-02 16:49:24 +0100

    Server Modified for Render Web Service

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
+const path = require("path");
 const express = require("express");
-const cors = require("cors");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "client", "dist")));
 
-app.get("/", (_, res) => {
-	res.send("Hello from Zuchy");
+// Catch All
+app.all("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => {
-	console.log(`Server Started @ ${PORT}`);
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server Started @ ${PORT}`));
